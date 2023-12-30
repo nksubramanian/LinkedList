0245f841a587d22f2e2f2b691da03435935f2e6d
subramanian <subramanian.kumarappan@halliburton.com>
2023-12-30 19:22:24 +0530

    palindrome done

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -6,24 +6,15 @@
         {
             LinkedListImp linkedListImp = new LinkedListImp();
             linkedListImp.AddNodeAtEnd(1);
-            linkedListImp.AddNodeAtEnd(2);
-            linkedListImp.AddNodeAtEnd(3);
-            linkedListImp.AddNodeAtBeginning(0);
-            linkedListImp.AddNodeAtBeginning(-1);
+            linkedListImp.AddNodeAtEnd(0);
+            linkedListImp.AddNodeAtEnd(1);
+            linkedListImp.AddNodeAtEnd(4);
+            linkedListImp.AddNodeAtBeginning(4);
             linkedListImp.PrintAll();
-            Console.WriteLine("The count is "+linkedListImp.GetLength());
-            linkedListImp.DeleteNodeAtBeginning();
-            Console.WriteLine("********************");
-            linkedListImp.PrintAll();
-            //Console.WriteLine("The count is " + linkedListImp.GetLength());
-            //Console.WriteLine($"Does 3 exist "+linkedListImp.IsKeyExists(3));
-            //Console.WriteLine($"Does 50 exist " + linkedListImp.IsKeyExists(50));
-            Console.WriteLine("Last node "+linkedListImp.GetLastNode(2));
-            linkedListImp.Reverse();
-            Console.WriteLine("********************Reversed*******************");
-            linkedListImp.PrintAll();
-            Console.WriteLine("Hello, World!");
-            linkedListImp.PrintReverse();
+
+
+             bool res = linkedListImp.IsPalindrome();
+            Console.WriteLine("gggggggggggg");
 
         }
     }
